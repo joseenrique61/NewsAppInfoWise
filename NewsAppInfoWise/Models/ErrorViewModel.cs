d2f08adc41f5114223b4c4e653e15c53a47f6b90
José Cerezo <joseenrique61@hotmail.com>
2025-12-10 20:34:08 -0500

    First commit

---
+++ NewsAppInfoWise/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace NewsAppInfoWise.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
